4c3a7f5eb4a275fc44a5077b256e3163385b0498
ilmanurh <ilma.hidayati@medicplus.co.id>
2022-03-18 18:52:02 +0700

    add logo item

---
+++ basic-vue/src/components/LogosItem.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="logos-wrapper">
+    <h2 class="title-header">Find Best Logo</h2>
+    <div class="item-logos">
+      <img class="logo" src="../assets/gopher.png" alt="Gopher" />
+    </div>
+    <div class="item-logos">
+      <img class="logo" src="../assets/docker.svg" alt="Docker" />
+    </div>
+    <div class="item-logos">
+      <img class="logo" src="../assets/gopher.png" alt="Gopher" />
+    </div>
+    <div class="item-logos">
+      <img class="logo" src="../assets/docker.svg" alt="Docker" />
+    </div>
+    <div class="item-logos">
+      <img class="logo" src="../assets/docker.svg" alt="Docker" />
+    </div>
+    <div class="item-logos">
+      <img class="logo" src="../assets/gopher.png" alt="Gopher" />
+    </div>
+    <div class="item-logos">
+      <img class="logo" src="../assets/docker.svg" alt="Docker" />
+    </div>
+    <div class="item-logos">
+      <img class="logo" src="../assets/gopher.png" alt="Gopher" />
+    </div>
+  </div>
+</template> 
+
+<script>
+export default {
+  name: "LogosItem",
+};
+</script>
+
+<style scoped>
+.title-header {
+  margin-bottom: 80px;
+  text-align: left;
+  margin-left: 50px;
+}
+
+.logos-wrapper {
+  width: 70%;
+  overflow: hidden;
+  margin: auto;
+}
+
+.item-logos {
+  width: 150px;
+  height: 150px;
+  float: left;
+  margin-left: 50px;
+}
+
+.item-logos .logo {
+  width: 100px;
+  height: 100px;
+}
+</style>
