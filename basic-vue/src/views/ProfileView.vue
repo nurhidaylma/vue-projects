d05c5a0b8a933544706ed32073a0a5a47e5ef68a
nurhidaylma <ilmanurhidayati752@gmail.com>
2022-04-06 10:33:29 +0700

    Add profile page

---
+++ basic-vue/src/views/ProfileView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="profile">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- msg is a props. It can be customized and only created once on the root file -->
+    <HelloWorld msg="This is a profile page"/> 
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'ProfileView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
