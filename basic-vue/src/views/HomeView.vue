b3a27b3fffd3af4b0130196bba3e368a1ca59642
nurhidaylma <ilmanurhidayati752@gmail.com>
2022-04-06 10:33:22 +0700

    Add home page

---
+++ basic-vue/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- msg is a props. It can be customized and only created once on the root file -->
+    <HelloWorld msg="This is a home page"/> 
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
